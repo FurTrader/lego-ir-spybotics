e63efa07f80e246cfff862428c4987c4d0a41ab1
FurTrader <32523605+FurTrader@users.noreply.github.com>
2026-06-28 11:25:25 -0400

    setup interrupts, move patterns to header

---
+++ src/IR_patterns.h
@@ -0,0 +1,323 @@
+#pragma once
+
+#include <Arduino.h>
+
+#define Ch1_OrangeNop_YellowNop_LEN 12
+uint16_t Ch1_OrangeNop_YellowNop[Ch1_OrangeNop_YellowNop_LEN]={
+  1040, 208, 208, 208, 208, 624, 208, 416, 208, 208, 1040, 416
+  };
+  
+#define Ch1_OrangeNop_YellowFwd_LEN 12
+uint16_t Ch1_OrangeNop_YellowFwd[Ch1_OrangeNop_YellowFwd_LEN]={
+  1040, 208, 208, 208, 208, 624, 624, 208, 208, 624, 208, 624
+  };
+
+#define Ch1_OrangeNop_YellowRev_LEN 8
+uint16_t Ch1_OrangeNop_YellowRev[Ch1_OrangeNop_YellowRev_LEN]={
+  1040, 208, 208, 208, 208, 624, 624, 1872
+  };
+
+#define Ch1_OrangeNop_YellowStp_LEN 12
+  uint16_t Ch1_OrangeNop_YellowStp[Ch1_OrangeNop_YellowStp_LEN]={
+  1040, 208, 208, 208, 208, 624, 208, 416, 1040, 208, 208, 416
+  };
+  
+#define Ch1_OrangeFwd_YellowNop_LEN 12
+uint16_t Ch1_OrangeFwd_YellowNop[Ch1_OrangeFwd_YellowNop_LEN]={
+  208, 624, 208, 208, 208, 208, 416, 416, 624, 208, 1248, 416
+  };
+
+#define Ch1_OrangeFwd_YellowFwd_LEN 14
+uint16_t Ch1_OrangeFwd_YellowFwd[Ch1_OrangeFwd_YellowFwd_LEN]={
+  208, 624, 208, 208, 208, 208, 416, 416, 208, 208, 208, 1040, 208, 624
+  };
+
+#define Ch1_OrangeFwd_YellowRev_LEN 14
+uint16_t Ch1_OrangeFwd_YellowRev[Ch1_OrangeFwd_YellowRev_LEN]={
+  208, 624, 208, 208, 208, 208, 416, 416, 208, 208, 208, 208, 208, 1456
+  };
+  
+#define Ch1_OrangeFwd_YellowStp_LEN 14
+uint16_t Ch1_OrangeFwd_YellowStp[Ch1_OrangeFwd_YellowStp_LEN]={
+  208, 624, 208, 208, 208, 208, 416, 416, 624, 416, 624, 208, 208, 416
+  };
+
+#define Ch1_OrangeRev_YellowNop_LEN 10
+uint16_t Ch1_OrangeRev_YellowNop[Ch1_OrangeRev_YellowNop_LEN]={
+  208, 1040, 208, 208, 208, 624, 624, 416, 832, 624
+  };  
+
+#define Ch1_OrangeRev_YellowFwd_LEN 14
+uint16_t Ch1_OrangeRev_YellowFwd[Ch1_OrangeRev_YellowFwd_LEN]={
+  208, 1040, 208, 208, 208, 624, 208, 208, 208, 208, 208, 624, 416, 416
+  };
+  
+#define Ch1_OrangeRev_YellowRev_LEN 12
+uint16_t Ch1_OrangeRev_YellowRev[Ch1_OrangeRev_YellowRev_LEN]={
+  208, 1040, 208, 208, 208, 624, 208, 208, 208, 1248, 208, 416
+  };
+
+#define Ch1_OrangeRev_YellowStp_LEN 10
+uint16_t Ch1_OrangeRev_YellowStp[Ch1_OrangeRev_YellowStp_LEN]={
+  208, 1040, 208, 208, 208, 624, 624, 208, 832, 832
+  };
+  
+#define Ch1_OrangeStp_YellowNop_LEN 10
+uint16_t Ch1_OrangeStp_YellowNop[Ch1_OrangeStp_YellowNop_LEN]={
+  832, 416, 208, 208, 416, 416, 208, 416, 1248, 624
+  };
+ 
+#define Ch1_OrangeStp_YellowFwd_LEN 10
+uint16_t Ch1_OrangeStp_YellowFwd[Ch1_OrangeStp_YellowFwd_LEN]={
+  832, 416, 208, 208, 416, 416, 624, 1040, 416, 416
+  };
+
+#define Ch1_OrangeStp_YellowRev_LEN 12
+uint16_t Ch1_OrangeStp_YellowRev[Ch1_OrangeStp_YellowRev_LEN]={
+  832, 416, 208, 208, 416, 416, 624, 208, 208, 832, 208, 416
+  };
+  
+#define Ch1_OrangeStp_YellowStp_LEN 12
+uint16_t Ch1_OrangeStp_YellowStp[Ch1_OrangeStp_YellowStp_LEN]={
+  832, 416, 208, 208, 416, 416, 208, 416, 208, 208, 624, 832
+  };
+
+#define Ch2_OrangeNop_YellowNop_LEN 12
+uint16_t Ch2_OrangeNop_YellowNop[Ch2_OrangeNop_YellowNop_LEN]={
+  1248, 416, 208, 624, 416, 208, 208, 208, 832, 624
+  };
+
+#define Ch2_OrangeNop_YellowFwd_LEN 10
+uint16_t Ch2_OrangeNop_YellowFwd[Ch2_OrangeNop_YellowFwd_LEN]={
+  1248, 416, 208, 624, 208, 416, 416, 624, 416, 416
+  }; 
+
+#define Ch2_OrangeNop_YellowRev_LEN 10
+uint16_t Ch2_OrangeNop_YellowRev[Ch2_OrangeNop_YellowRev_LEN]={
+  1248, 416, 208, 624, 208, 416, 208, 1040, 208, 416
+  };
+
+#define Ch2_OrangeNop_YellowStp_LEN 8
+uint16_t Ch2_OrangeNop_YellowStp[Ch2_OrangeNop_YellowStp_LEN]={
+  1248, 416, 208, 624, 416, 208, 1040, 832
+  };
+
+#define Ch2_OrangeFwd_YellowNop_LEN 10
+uint16_t Ch2_OrangeFwd_YellowNop[Ch2_OrangeFwd_YellowNop_LEN]={
+  208, 624, 416, 416, 416, 416, 208, 416, 1248, 624
+  };
+
+#define Ch2_OrangeFwd_YellowFwd_LEN 10
+uint16_t Ch2_OrangeFwd_YellowFwd[Ch2_OrangeFwd_YellowFwd_LEN]={
+  208, 624, 416, 416, 416, 416, 624, 1040, 416, 416
+  };
+
+#define Ch2_OrangeFwd_YellowRev_LEN 12
+uint16_t Ch2_OrangeFwd_YellowRev[Ch2_OrangeFwd_YellowRev_LEN]={
+  208, 624, 416, 416, 416, 416, 624, 208, 208, 832, 208, 416
+  };
+
+#define Ch2_OrangeFwd_YellowStp_LEN 12
+uint16_t Ch2_OrangeFwd_YellowStp[Ch2_OrangeFwd_YellowStp_LEN]={
+  208, 624, 416, 416, 416, 416, 208, 416, 208, 208, 624, 832
+  };
+
+#define Ch2_OrangeRev_YellowNop_LEN 12
+uint16_t Ch2_OrangeRev_YellowNop[Ch2_OrangeRev_YellowNop_LEN]={
+  208, 832, 208, 416, 208, 624, 208, 416, 208, 208, 1040, 416
+  };
+
+#define Ch2_OrangeRev_YellowFwd_LEN 12
+uint16_t Ch2_OrangeRev_YellowFwd[Ch2_OrangeRev_YellowFwd_LEN]={
+  208, 832, 208, 416, 208, 624, 624, 208, 208, 624, 208, 624
+  };
+
+#define Ch2_OrangeRev_YellowRev_LEN 8
+uint16_t Ch2_OrangeRev_YellowRev[Ch2_OrangeRev_YellowRev_LEN]={
+  208, 832, 208, 416, 208, 624, 624, 1872
+  };
+
+#define Ch2_OrangeRev_YellowStp_LEN 12
+uint16_t Ch2_OrangeRev_YellowStp[Ch2_OrangeRev_YellowStp_LEN]={
+  208, 832, 208, 416, 208, 624, 208, 416, 1040, 208, 208, 416
+  };
+
+#define Ch2_OrangeStp_YellowNop_LEN 10
+uint16_t Ch2_OrangeStp_YellowNop[Ch2_OrangeStp_YellowNop_LEN]={
+  832, 208, 208, 416, 416, 416, 416, 208, 1456, 416
+  };
+
+#define Ch2_OrangeStp_YellowFwd_LEN 12
+uint16_t Ch2_OrangeStp_YellowFwd[Ch2_OrangeStp_YellowFwd_LEN]={
+  832, 208, 208, 416, 416, 416, 208, 416, 208, 832, 208, 624
+  };
+
+#define Ch2_OrangeStp_YellowRev_LEN 10
+uint16_t Ch2_OrangeStp_YellowRev[Ch2_OrangeStp_YellowRev_LEN]={
+  832, 208, 208, 416, 416, 416, 208, 416, 416, 1456
+  };
+  
+#define Ch2_OrangeStp_YellowStp_LEN 14
+uint16_t Ch2_OrangeStp_YellowStp[Ch2_OrangeStp_YellowStp_LEN]={
+  832, 208, 208, 416, 416, 416, 416, 208, 208, 208, 624, 208, 208, 416
+  };
+
+#define Ch3_OrangeNop_YellowNop_LEN 10
+uint16_t Ch3_OrangeNop_YellowNop[Ch3_OrangeNop_YellowNop_LEN]={
+  1040, 624, 416, 416, 208, 208, 416, 208, 832, 624
+  };
+
+#define Ch3_OrangeNop_YellowFwd_LEN 10
+uint16_t Ch3_OrangeNop_YellowFwd[Ch3_OrangeNop_YellowFwd_LEN]={
+  1040, 624, 416, 416, 416, 208, 416, 624, 208, 624
+  };
+
+#define Ch3_OrangeNop_YellowRev_LEN 8
+uint16_t Ch3_OrangeNop_YellowRev[Ch3_OrangeNop_YellowRev_LEN]={
+  1040, 624, 416, 416, 416, 208, 208, 1664
+  };
+
+#define Ch3_OrangeNop_YellowStp_LEN 8
+uint16_t Ch3_OrangeNop_YellowStp[Ch3_OrangeNop_YellowStp_LEN]={
+  1040, 624, 416, 416, 208, 208, 1248, 832
+  };
+
+#define Ch3_OrangeFwd_YellowNop_LEN 10
+uint16_t Ch3_OrangeFwd_YellowNop[Ch3_OrangeFwd_YellowNop_LEN]={
+  208, 624, 208, 624, 208, 624, 416, 208, 1456, 416
+  };
+
+#define Ch3_OrangeFwd_YellowFwd_LEN 12
+uint16_t Ch3_OrangeFwd_YellowFwd[Ch3_OrangeFwd_YellowFwd_LEN]={
+  208, 624, 208, 624, 208, 624, 208, 416, 208, 832, 208, 624
+  };
+
+#define Ch3_OrangeFwd_YellowRev_LEN 10
+uint16_t Ch3_OrangeFwd_YellowRev[Ch3_OrangeFwd_YellowRev_LEN]={
+  208, 624, 208, 624, 208, 624, 208, 416, 416, 1456
+  };
+
+#define Ch3_OrangeFwd_YellowStp_LEN 14
+uint16_t Ch3_OrangeFwd_YellowStp[Ch3_OrangeFwd_YellowStp_LEN]={
+  208, 624, 208, 624, 208, 624, 416, 208, 208, 208, 624, 208, 208, 416
+  };
+
+#define Ch3_OrangeRev_YellowNop_LEN 10
+uint16_t Ch3_OrangeRev_YellowNop[Ch3_OrangeRev_YellowNop_LEN]={
+  208, 1456, 416, 416, 416, 208, 208, 208, 832, 624
+  };
+
+#define Ch3_OrangeRev_YellowFwd_LEN 10
+uint16_t Ch3_OrangeRev_YellowFwd[Ch3_OrangeRev_YellowFwd_LEN]={
+  208, 1456, 416, 416, 208, 416, 416, 624, 416, 416
+  };
+
+#define Ch3_OrangeRev_YellowRev_LEN 10
+uint16_t Ch3_OrangeRev_YellowRev[Ch3_OrangeRev_YellowRev_LEN]={
+  208, 1456, 416, 416, 208, 416, 208, 1040, 208, 416
+  };
+
+#define Ch3_OrangeRev_YellowStp_LEN 8
+uint16_t Ch3_OrangeRev_YellowStp[Ch3_OrangeRev_YellowStp_LEN]={
+  208, 1456, 416, 416, 416, 208, 1040, 832
+  };
+
+#define Ch3_OrangeStp_YellowNop_LEN 8
+uint16_t Ch3_OrangeStp_YellowNop[Ch3_OrangeStp_YellowNop_LEN]={
+  832, 832, 208, 624, 208, 208, 1664, 416
+  };
+
+#define Ch3_OrangeStp_YellowFwd_LEN 10
+uint16_t Ch3_OrangeStp_YellowFwd[Ch3_OrangeStp_YellowFwd_LEN]={
+  832, 832, 208, 624, 416, 208, 208, 832, 416, 416
+  };
+
+#define Ch3_OrangeStp_YellowRev_LEN 10
+uint16_t Ch3_OrangeStp_YellowRev[Ch3_OrangeStp_YellowRev_LEN]={
+  832, 832, 208, 624, 416, 208, 416, 832, 208, 416
+  };
+
+#define Ch3_OrangeStp_YellowStp_LEN 12
+uint16_t Ch3_OrangeStp_YellowStp[Ch3_OrangeStp_YellowStp_LEN]={
+  832, 832, 208, 624, 208, 208, 416, 208, 624, 208, 208, 416
+  };
+
+#define All_OrangeNop_YellowNop_LEN 8
+uint16_t All_OrangeNop_YellowNop[All_OrangeNop_YellowNop_LEN]={
+  1456, 208, 416, 416, 624, 416, 832, 624
+  };
+
+#define All_OrangeNop_YellowFwd_LEN 12
+uint16_t All_OrangeNop_YellowFwd[All_OrangeNop_YellowFwd_LEN]={
+  1456, 208, 416, 416, 208, 208, 208, 208, 208, 624, 416, 416
+  };
+  
+#define All_OrangeNop_YellowRev_LEN 10
+uint16_t All_OrangeNop_YellowRev[All_OrangeNop_YellowRev_LEN]={
+  1456, 208, 416, 416, 208, 208, 208, 1248, 208, 416
+  };
+
+#define All_OrangeNop_YellowStp_LEN 8
+uint16_t All_OrangeNop_YellowStp[All_OrangeNop_YellowStp_LEN]={
+  1456, 208, 416, 416, 624, 208, 832, 832
+  };
+
+#define All_OrangeFwd_YellowNop_LEN 12
+uint16_t All_OrangeFwd_YellowNop[All_OrangeFwd_YellowNop_LEN]={
+  208, 624, 624, 208, 208, 624, 208, 208, 208, 208, 1040, 624
+  };
+
+#define All_OrangeFwd_YellowFwd_LEN 10
+uint16_t All_OrangeFwd_YellowFwd[All_OrangeFwd_YellowFwd_LEN]={
+  208, 624, 624, 208, 208, 624, 416, 1248, 208, 624
+  };
+
+#define All_OrangeFwd_YellowRev_LEN 10
+uint16_t All_OrangeFwd_YellowRev[All_OrangeFwd_YellowRev_LEN]={
+  208, 624, 624, 208, 208, 624, 416, 416, 208, 1456
+  };
+
+#define All_OrangeFwd_YellowStp_LEN 12
+uint16_t All_OrangeFwd_YellowStp[All_OrangeFwd_YellowStp_LEN]={
+  208, 624, 624, 208, 208, 624, 208, 208, 208, 416, 624, 832
+  };
+
+#define All_OrangeRev_YellowNop_LEN 12
+uint16_t All_OrangeRev_YellowNop[All_OrangeRev_YellowNop_LEN]={
+  208, 832, 416, 208, 416, 416, 208, 208, 208, 416, 1040, 416
+  };
+
+#define All_OrangeRev_YellowFwd_LEN 12
+uint16_t All_OrangeRev_YellowFwd[All_OrangeRev_YellowFwd_LEN]={
+  208, 832, 416, 208, 416, 416, 416, 416, 208, 624, 416, 416
+  };
+
+#define All_OrangeRev_YellowRev_LEN 10
+uint16_t All_OrangeRev_YellowRev[All_OrangeRev_YellowRev_LEN]={
+  208, 832, 416, 208, 416, 416, 416, 1456, 208, 416
+  };
+
+#define All_OrangeRev_YellowStp_LEN 14
+uint16_t All_OrangeRev_YellowStp[All_OrangeRev_YellowStp_LEN]={
+  208, 832, 416, 208, 416, 416, 208, 208, 208, 208, 832, 208, 208, 416
+  };
+
+#define All_OrangeStp_YellowNop_LEN 10
+uint16_t All_OrangeStp_YellowNop[All_OrangeStp_YellowNop_LEN]={
+  832, 208, 416, 208, 208, 624, 624, 208, 1248, 416
+  };
+
+#define All_OrangeStp_YellowFwd_LEN 12
+uint16_t All_OrangeStp_YellowFwd[All_OrangeStp_YellowFwd_LEN]={
+  832, 208, 416, 208, 208, 624, 208, 208, 208, 1040, 208, 624
+  };
+
+#define All_OrangeStp_YellowRev_LEN 12
+uint16_t All_OrangeStp_YellowRev[All_OrangeStp_YellowRev_LEN]={
+  832, 208, 416, 208, 208, 624, 208, 208, 208, 208, 208, 1456
+  };
+
+#define All_OrangeStp_YellowStp_LEN 12
+uint16_t All_OrangeStp_YellowStp[All_OrangeStp_YellowStp_LEN]={
+  832, 208, 416, 208, 208, 624, 624, 416, 624, 208, 208, 416
+  };
--- src/lego-ir-spybotics.cpp
+++ src/lego-ir-spybotics.cpp
@@ -6,412 +6,127 @@
 #include <Arduino.h>
 #include "IRLibSendBase.h"    //We need the base code
 #include "IRLib_HashRaw.h"    //Only use raw sender
+#include "IR_patterns.h"
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
 
 IRsendRaw mySender;
 
-#define Ch1_OrangeNop_YellowNop_LEN 12
-uint16_t Ch1_OrangeNop_YellowNop[Ch1_OrangeNop_YellowNop_LEN]={
-  1040, 208, 208, 208, 208, 624, 208, 416, 208, 208, 1040, 416
-  };
-  
-#define Ch1_OrangeNop_YellowFwd_LEN 12
-uint16_t Ch1_OrangeNop_YellowFwd[Ch1_OrangeNop_YellowFwd_LEN]={
-  1040, 208, 208, 208, 208, 624, 624, 208, 208, 624, 208, 624
-  };
+//The TX and RX LEDs are available if we aren't using USB
+// TXLED0  PORTD |= (1<<5)   // TX off
+// TXLED1  PORTD &= ~(1<<5)  // TX on
+// RXLED0  PORTB |= (1<<0)   // RX off
+// RXLED1  PORTB &= ~(1<<0)  // RX on
 
-#define Ch1_OrangeNop_YellowRev_LEN 8
-uint16_t Ch1_OrangeNop_YellowRev[Ch1_OrangeNop_YellowRev_LEN]={
-  1040, 208, 208, 208, 208, 624, 624, 1872
-  };
+#define PROGRAMMING_PIN 4 // ground this pin to disable power saving modes
 
-#define Ch1_OrangeNop_YellowStp_LEN 12
-  uint16_t Ch1_OrangeNop_YellowStp[Ch1_OrangeNop_YellowStp_LEN]={
-  1040, 208, 208, 208, 208, 624, 208, 416, 1040, 208, 208, 416
-  };
-  
-#define Ch1_OrangeFwd_YellowNop_LEN 12
-uint16_t Ch1_OrangeFwd_YellowNop[Ch1_OrangeFwd_YellowNop_LEN]={
-  208, 624, 208, 208, 208, 208, 416, 416, 624, 208, 1248, 416
-  };
-
-#define Ch1_OrangeFwd_YellowFwd_LEN 14
-uint16_t Ch1_OrangeFwd_YellowFwd[Ch1_OrangeFwd_YellowFwd_LEN]={
-  208, 624, 208, 208, 208, 208, 416, 416, 208, 208, 208, 1040, 208, 624
-  };
-
-#define Ch1_OrangeFwd_YellowRev_LEN 14
-uint16_t Ch1_OrangeFwd_YellowRev[Ch1_OrangeFwd_YellowRev_LEN]={
-  208, 624, 208, 208, 208, 208, 416, 416, 208, 208, 208, 208, 208, 1456
-  };
-  
-#define Ch1_OrangeFwd_YellowStp_LEN 14
-uint16_t Ch1_OrangeFwd_YellowStp[Ch1_OrangeFwd_YellowStp_LEN]={
-  208, 624, 208, 208, 208, 208, 416, 416, 624, 416, 624, 208, 208, 416
-  };
-
-#define Ch1_OrangeRev_YellowNop_LEN 10
-uint16_t Ch1_OrangeRev_YellowNop[Ch1_OrangeRev_YellowNop_LEN]={
-  208, 1040, 208, 208, 208, 624, 624, 416, 832, 624
-  };  
-
-#define Ch1_OrangeRev_YellowFwd_LEN 14
-uint16_t Ch1_OrangeRev_YellowFwd[Ch1_OrangeRev_YellowFwd_LEN]={
-  208, 1040, 208, 208, 208, 624, 208, 208, 208, 208, 208, 624, 416, 416
-  };
-  
-#define Ch1_OrangeRev_YellowRev_LEN 12
-uint16_t Ch1_OrangeRev_YellowRev[Ch1_OrangeRev_YellowRev_LEN]={
-  208, 1040, 208, 208, 208, 624, 208, 208, 208, 1248, 208, 416
-  };
-
-#define Ch1_OrangeRev_YellowStp_LEN 10
-uint16_t Ch1_OrangeRev_YellowStp[Ch1_OrangeRev_YellowStp_LEN]={
-  208, 1040, 208, 208, 208, 624, 624, 208, 832, 832
-  };
-  
-#define Ch1_OrangeStp_YellowNop_LEN 10
-uint16_t Ch1_OrangeStp_YellowNop[Ch1_OrangeStp_YellowNop_LEN]={
-  832, 416, 208, 208, 416, 416, 208, 416, 1248, 624
-  };
- 
-#define Ch1_OrangeStp_YellowFwd_LEN 10
-uint16_t Ch1_OrangeStp_YellowFwd[Ch1_OrangeStp_YellowFwd_LEN]={
-  832, 416, 208, 208, 416, 416, 624, 1040, 416, 416
-  };
-
-#define Ch1_OrangeStp_YellowRev_LEN 12
-uint16_t Ch1_OrangeStp_YellowRev[Ch1_OrangeStp_YellowRev_LEN]={
-  832, 416, 208, 208, 416, 416, 624, 208, 208, 832, 208, 416
-  };
-  
-#define Ch1_OrangeStp_YellowStp_LEN 12
-uint16_t Ch1_OrangeStp_YellowStp[Ch1_OrangeStp_YellowStp_LEN]={
-  832, 416, 208, 208, 416, 416, 208, 416, 208, 208, 624, 832
-  };
-
-#define Ch2_OrangeNop_YellowNop_LEN 12
-uint16_t Ch2_OrangeNop_YellowNop[Ch2_OrangeNop_YellowNop_LEN]={
-  1248, 416, 208, 624, 416, 208, 208, 208, 832, 624
-  };
-
-#define Ch2_OrangeNop_YellowFwd_LEN 10
-uint16_t Ch2_OrangeNop_YellowFwd[Ch2_OrangeNop_YellowFwd_LEN]={
-  1248, 416, 208, 624, 208, 416, 416, 624, 416, 416
-  }; 
-
-#define Ch2_OrangeNop_YellowRev_LEN 10
-uint16_t Ch2_OrangeNop_YellowRev[Ch2_OrangeNop_YellowRev_LEN]={
-  1248, 416, 208, 624, 208, 416, 208, 1040, 208, 416
-  };
-
-#define Ch2_OrangeNop_YellowStp_LEN 8
-uint16_t Ch2_OrangeNop_YellowStp[Ch2_OrangeNop_YellowStp_LEN]={
-  1248, 416, 208, 624, 416, 208, 1040, 832
-  };
-
-#define Ch2_OrangeFwd_YellowNop_LEN 10
-uint16_t Ch2_OrangeFwd_YellowNop[Ch2_OrangeFwd_YellowNop_LEN]={
-  208, 624, 416, 416, 416, 416, 208, 416, 1248, 624
-  };
-
-#define Ch2_OrangeFwd_YellowFwd_LEN 10
-uint16_t Ch2_OrangeFwd_YellowFwd[Ch2_OrangeFwd_YellowFwd_LEN]={
-  208, 624, 416, 416, 416, 416, 624, 1040, 416, 416
-  };
-
-#define Ch2_OrangeFwd_YellowRev_LEN 12
-uint16_t Ch2_OrangeFwd_YellowRev[Ch2_OrangeFwd_YellowRev_LEN]={
-  208, 624, 416, 416, 416, 416, 624, 208, 208, 832, 208, 416
-  };
-
-#define Ch2_OrangeFwd_YellowStp_LEN 12
-uint16_t Ch2_OrangeFwd_YellowStp[Ch2_OrangeFwd_YellowStp_LEN]={
-  208, 624, 416, 416, 416, 416, 208, 416, 208, 208, 624, 832
-  };
-
-#define Ch2_OrangeRev_YellowNop_LEN 12
-uint16_t Ch2_OrangeRev_YellowNop[Ch2_OrangeRev_YellowNop_LEN]={
-  208, 832, 208, 416, 208, 624, 208, 416, 208, 208, 1040, 416
-  };
-
-#define Ch2_OrangeRev_YellowFwd_LEN 12
-uint16_t Ch2_OrangeRev_YellowFwd[Ch2_OrangeRev_YellowFwd_LEN]={
-  208, 832, 208, 416, 208, 624, 624, 208, 208, 624, 208, 624
-  };
-
-#define Ch2_OrangeRev_YellowRev_LEN 8
-uint16_t Ch2_OrangeRev_YellowRev[Ch2_OrangeRev_YellowRev_LEN]={
-  208, 832, 208, 416, 208, 624, 624, 1872
-  };
-
-#define Ch2_OrangeRev_YellowStp_LEN 12
-uint16_t Ch2_OrangeRev_YellowStp[Ch2_OrangeRev_YellowStp_LEN]={
-  208, 832, 208, 416, 208, 624, 208, 416, 1040, 208, 208, 416
-  };
-
-#define Ch2_OrangeStp_YellowNop_LEN 10
-uint16_t Ch2_OrangeStp_YellowNop[Ch2_OrangeStp_YellowNop_LEN]={
-  832, 208, 208, 416, 416, 416, 416, 208, 1456, 416
-  };
-
-#define Ch2_OrangeStp_YellowFwd_LEN 12
-uint16_t Ch2_OrangeStp_YellowFwd[Ch2_OrangeStp_YellowFwd_LEN]={
-  832, 208, 208, 416, 416, 416, 208, 416, 208, 832, 208, 624
-  };
-
-#define Ch2_OrangeStp_YellowRev_LEN 10
-uint16_t Ch2_OrangeStp_YellowRev[Ch2_OrangeStp_YellowRev_LEN]={
-  832, 208, 208, 416, 416, 416, 208, 416, 416, 1456
-  };
-  
-#define Ch2_OrangeStp_YellowStp_LEN 14
-uint16_t Ch2_OrangeStp_YellowStp[Ch2_OrangeStp_YellowStp_LEN]={
-  832, 208, 208, 416, 416, 416, 416, 208, 208, 208, 624, 208, 208, 416
-  };
-
-#define Ch3_OrangeNop_YellowNop_LEN 10
-uint16_t Ch3_OrangeNop_YellowNop[Ch3_OrangeNop_YellowNop_LEN]={
-  1040, 624, 416, 416, 208, 208, 416, 208, 832, 624
-  };
-
-#define Ch3_OrangeNop_YellowFwd_LEN 10
-uint16_t Ch3_OrangeNop_YellowFwd[Ch3_OrangeNop_YellowFwd_LEN]={
-  1040, 624, 416, 416, 416, 208, 416, 624, 208, 624
-  };
-
-#define Ch3_OrangeNop_YellowRev_LEN 8
-uint16_t Ch3_OrangeNop_YellowRev[Ch3_OrangeNop_YellowRev_LEN]={
-  1040, 624, 416, 416, 416, 208, 208, 1664
-  };
-
-#define Ch3_OrangeNop_YellowStp_LEN 8
-uint16_t Ch3_OrangeNop_YellowStp[Ch3_OrangeNop_YellowStp_LEN]={
-  1040, 624, 416, 416, 208, 208, 1248, 832
-  };
-
-#define Ch3_OrangeFwd_YellowNop_LEN 10
-uint16_t Ch3_OrangeFwd_YellowNop[Ch3_OrangeFwd_YellowNop_LEN]={
-  208, 624, 208, 624, 208, 624, 416, 208, 1456, 416
-  };
-
-#define Ch3_OrangeFwd_YellowFwd_LEN 12
-uint16_t Ch3_OrangeFwd_YellowFwd[Ch3_OrangeFwd_YellowFwd_LEN]={
-  208, 624, 208, 624, 208, 624, 208, 416, 208, 832, 208, 624
-  };
-
-#define Ch3_OrangeFwd_YellowRev_LEN 10
-uint16_t Ch3_OrangeFwd_YellowRev[Ch3_OrangeFwd_YellowRev_LEN]={
-  208, 624, 208, 624, 208, 624, 208, 416, 416, 1456
-  };
-
-#define Ch3_OrangeFwd_YellowStp_LEN 14
-uint16_t Ch3_OrangeFwd_YellowStp[Ch3_OrangeFwd_YellowStp_LEN]={
-  208, 624, 208, 624, 208, 624, 416, 208, 208, 208, 624, 208, 208, 416
-  };
-
-#define Ch3_OrangeRev_YellowNop_LEN 10
-uint16_t Ch3_OrangeRev_YellowNop[Ch3_OrangeRev_YellowNop_LEN]={
-  208, 1456, 416, 416, 416, 208, 208, 208, 832, 624
-  };
-
-#define Ch3_OrangeRev_YellowFwd_LEN 10
-uint16_t Ch3_OrangeRev_YellowFwd[Ch3_OrangeRev_YellowFwd_LEN]={
-  208, 1456, 416, 416, 208, 416, 416, 624, 416, 416
-  };
-
-#define Ch3_OrangeRev_YellowRev_LEN 10
-uint16_t Ch3_OrangeRev_YellowRev[Ch3_OrangeRev_YellowRev_LEN]={
-  208, 1456, 416, 416, 208, 416, 208, 1040, 208, 416
-  };
-
-#define Ch3_OrangeRev_YellowStp_LEN 8
-uint16_t Ch3_OrangeRev_YellowStp[Ch3_OrangeRev_YellowStp_LEN]={
-  208, 1456, 416, 416, 416, 208, 1040, 832
-  };
-
-#define Ch3_OrangeStp_YellowNop_LEN 8
-uint16_t Ch3_OrangeStp_YellowNop[Ch3_OrangeStp_YellowNop_LEN]={
-  832, 832, 208, 624, 208, 208, 1664, 416
-  };
-
-#define Ch3_OrangeStp_YellowFwd_LEN 10
-uint16_t Ch3_OrangeStp_YellowFwd[Ch3_OrangeStp_YellowFwd_LEN]={
-  832, 832, 208, 624, 416, 208, 208, 832, 416, 416
-  };
-
-#define Ch3_OrangeStp_YellowRev_LEN 10
-uint16_t Ch3_OrangeStp_YellowRev[Ch3_OrangeStp_YellowRev_LEN]={
-  832, 832, 208, 624, 416, 208, 416, 832, 208, 416
-  };
-
-#define Ch3_OrangeStp_YellowStp_LEN 12
-uint16_t Ch3_OrangeStp_YellowStp[Ch3_OrangeStp_YellowStp_LEN]={
-  832, 832, 208, 624, 208, 208, 416, 208, 624, 208, 208, 416
-  };
-
-#define All_OrangeNop_YellowNop_LEN 8
-uint16_t All_OrangeNop_YellowNop[All_OrangeNop_YellowNop_LEN]={
-  1456, 208, 416, 416, 624, 416, 832, 624
-  };
-
-#define All_OrangeNop_YellowFwd_LEN 12
-uint16_t All_OrangeNop_YellowFwd[All_OrangeNop_YellowFwd_LEN]={
-  1456, 208, 416, 416, 208, 208, 208, 208, 208, 624, 416, 416
-  };
-  
-#define All_OrangeNop_YellowRev_LEN 10
-uint16_t All_OrangeNop_YellowRev[All_OrangeNop_YellowRev_LEN]={
-  1456, 208, 416, 416, 208, 208, 208, 1248, 208, 416
-  };
-
-#define All_OrangeNop_YellowStp_LEN 8
-uint16_t All_OrangeNop_YellowStp[All_OrangeNop_YellowStp_LEN]={
-  1456, 208, 416, 416, 624, 208, 832, 832
-  };
-
-#define All_OrangeFwd_YellowNop_LEN 12
-uint16_t All_OrangeFwd_YellowNop[All_OrangeFwd_YellowNop_LEN]={
-  208, 624, 624, 208, 208, 624, 208, 208, 208, 208, 1040, 624
-  };
-
-#define All_OrangeFwd_YellowFwd_LEN 10
-uint16_t All_OrangeFwd_YellowFwd[All_OrangeFwd_YellowFwd_LEN]={
-  208, 624, 624, 208, 208, 624, 416, 1248, 208, 624
-  };
-
-#define All_OrangeFwd_YellowRev_LEN 10
-uint16_t All_OrangeFwd_YellowRev[All_OrangeFwd_YellowRev_LEN]={
-  208, 624, 624, 208, 208, 624, 416, 416, 208, 1456
-  };
-
-#define All_OrangeFwd_YellowStp_LEN 12
-uint16_t All_OrangeFwd_YellowStp[All_OrangeFwd_YellowStp_LEN]={
-  208, 624, 624, 208, 208, 624, 208, 208, 208, 416, 624, 832
-  };
-
-#define All_OrangeRev_YellowNop_LEN 12
-uint16_t All_OrangeRev_YellowNop[All_OrangeRev_YellowNop_LEN]={
-  208, 832, 416, 208, 416, 416, 208, 208, 208, 416, 1040, 416
-  };
-
-#define All_OrangeRev_YellowFwd_LEN 12
-uint16_t All_OrangeRev_YellowFwd[All_OrangeRev_YellowFwd_LEN]={
-  208, 832, 416, 208, 416, 416, 416, 416, 208, 624, 416, 416
-  };
-
-#define All_OrangeRev_YellowRev_LEN 10
-uint16_t All_OrangeRev_YellowRev[All_OrangeRev_YellowRev_LEN]={
-  208, 832, 416, 208, 416, 416, 416, 1456, 208, 416
-  };
-
-#define All_OrangeRev_YellowStp_LEN 14
-uint16_t All_OrangeRev_YellowStp[All_OrangeRev_YellowStp_LEN]={
-  208, 832, 416, 208, 416, 416, 208, 208, 208, 208, 832, 208, 208, 416
-  };
-
-#define All_OrangeStp_YellowNop_LEN 10
-uint16_t All_OrangeStp_YellowNop[All_OrangeStp_YellowNop_LEN]={
-  832, 208, 416, 208, 208, 624, 624, 208, 1248, 416
-  };
-
-#define All_OrangeStp_YellowFwd_LEN 12
-uint16_t All_OrangeStp_YellowFwd[All_OrangeStp_YellowFwd_LEN]={
-  832, 208, 416, 208, 208, 624, 208, 208, 208, 1040, 208, 624
-  };
-
-#define All_OrangeStp_YellowRev_LEN 12
-uint16_t All_OrangeStp_YellowRev[All_OrangeStp_YellowRev_LEN]={
-  832, 208, 416, 208, 208, 624, 208, 208, 208, 208, 208, 1456
-  };
-
-#define All_OrangeStp_YellowStp_LEN 12
-uint16_t All_OrangeStp_YellowStp[All_OrangeStp_YellowStp_LEN]={
-  832, 208, 416, 208, 208, 624, 624, 416, 624, 208, 208, 416
-  };
-
-#define PROGRAMMING_PIN 2 // ground this pin to disable power saving modes
-
-#define LEFT_FWD_PB 3
-#define LEFT_REV_PB 3
-#define LEFT_FWD_PB 3
-#define LEFT_REV_PB 3
+#define LEFT_FWD_PB  3   // INT0 / PD0
+#define LEFT_REV_PB  2   // INT1 / PD1
+#define RIGHT_FWD_PB 0   // INT2 / PD2
+#define RIGHT_REV_PB 1   // INT3 / PD3
 
 bool programming_mode = false;
 
-void setup() {
-  pinMode(PROGRAMMING_PIN, INPUT_PULLUP);
-  if (digitalRead(PROGRAMMING_PIN)){
-    programming_mode = true;
-  }
+volatile uint8_t wakeSource = 0;
 
-  Serial.begin(9600);
-  delay(200); 
-  while (!Serial); //delay for Leonardo
-  Serial.println("Every time you press a key is a serial monitor we will send.");
+// ── ISRs: disable themselves immediately, just record which button ──
+ISR(INT0_vect) { EIMSK &= ~(1 << INT0); wakeSource = LEFT_FWD_PB; }
+ISR(INT1_vect) { EIMSK &= ~(1 << INT1); wakeSource = LEFT_REV_PB; }
+ISR(INT2_vect) { EIMSK &= ~(1 << INT2); wakeSource = RIGHT_FWD_PB; }
+ISR(INT3_vect) { EIMSK &= ~(1 << INT3); wakeSource = RIGHT_REV_PB; }
+
+// enable the interrupts to prepare for sleep
+void enableWakeInterrupts() {
+    // Trigger on LOW level
+    EICRA = 0x00;   // INT0–INT3 all set to LOW level (00 = low level)
+    // Clear any stale flags first
+    EIFR = (1 << INTF0) | (1 << INTF1) | (1 << INTF2) | (1 << INTF3);
+    // Enable all four
+    EIMSK |= (1 << INT0) | (1 << INT1) | (1 << INT2) | (1 << INT3);
 }
 
+void enterSleep() {
+    // Power down peripherals you don't need
+    TXLED0;
+    power_usart0_disable();
+    power_spi_disable();
+    power_twi_disable();
+    power_timer0_disable();
+    power_timer1_disable();
+    power_timer2_disable();
+    power_timer3_disable();
 
-#define WAIT 25
+    enableWakeInterrupts();
+
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+
+    sei();              // interrupts must be on to wake
+    sleep_cpu();        // ← CPU halts here
+
+    // ── resumes here after ISR runs ──
+    sleep_disable();
+
+    // Re-enable peripherals as needed
+    power_timer0_enable();  // millis() needs this
+    //power_usart0_enable();
+    //power_spi_enable();
+    //power_twi_enable();
+
+    power_timer1_enable();
+    power_timer2_enable();
+    power_timer3_enable();
+
+    TXLED1; // turn on the tx led
+}
+
+void handleButton(uint8_t pin) {
+    if (digitalRead(pin) == LOW) {
+        // still held, process it
+    }
+}
+
+void setup() {
+  pinMode(PROGRAMMING_PIN, INPUT_PULLUP);
+  pinMode(LEFT_FWD_PB, INPUT_PULLUP);
+  pinMode(LEFT_REV_PB, INPUT_PULLUP);
+  pinMode(RIGHT_FWD_PB, INPUT_PULLUP);
+  pinMode(RIGHT_REV_PB, INPUT_PULLUP);
+
+  if (!digitalRead(PROGRAMMING_PIN)){
+    programming_mode = true;
+    Serial.begin(9600);
+    delay(200); 
+    while (!Serial); //delay for Leonardo
+    Serial.println("Programming mode is active, CPU will not sleep.");
+  }else{
+    // set up to use usb leds
+    // Disable USB to save power and free the LED pins
+    USBCON |= (1 << FRZCLK);   // freeze USB clock
+    PLLCSR &= ~(1 << PLLE);    // stop PLL
+    USBCON &= ~(1 << USBE);    // disable USB
+
+    // Now take full control of both LED pins
+    DDRB |= (1 << PB0);
+    DDRD |= (1 << PD5);
+
+    // Start with both off (active LOW)
+    PORTB |= (1 << PB0);
+    PORTD |= (1 << PD5);
+
+    TXLED1; // turn on the tx led
+  }
+}
 
 void loop() {
-  if (Serial.available()) {
-    int serialbyte = Serial.read();
-    Serial.print("case '");
-    Serial.write(serialbyte);   // prints the character exactly
-    Serial.println("':");
-
-    switch(serialbyte) {
-      
-      case 'G':
-        mySender.send(All_OrangeNop_YellowNop,All_OrangeNop_YellowNop_LEN,76);
-        delay(WAIT);
-        break;
-
-      case 'F':
-        mySender.send(All_OrangeFwd_YellowFwd,All_OrangeFwd_YellowFwd_LEN,76);
-        delay(WAIT);
-        break;
-
-      case 'B':
-        mySender.send(All_OrangeRev_YellowRev,All_OrangeRev_YellowRev_LEN,76);
-        delay(WAIT);
-        break;
-
-      case 'L':
-        mySender.send(All_OrangeFwd_YellowRev,All_OrangeFwd_YellowRev_LEN,76);
-        delay(WAIT);
-        break;
-
-      case 'R':
-        mySender.send(All_OrangeRev_YellowFwd,All_OrangeRev_YellowFwd_LEN,76);
-        delay(WAIT);
-        break;
-
-      case '1':
-        mySender.send(All_OrangeNop_YellowFwd,All_OrangeNop_YellowFwd_LEN,76);
-        delay(WAIT);
-        break;
-
-      case '0':
-        mySender.send(All_OrangeNop_YellowRev,All_OrangeNop_YellowRev_LEN,76);
-        delay(WAIT);
-        break;
-
-      case '2':
-        mySender.send(All_OrangeFwd_YellowNop,All_OrangeFwd_YellowNop_LEN,76);
-        delay(WAIT);
-        break;
-
-      case '3':
-        mySender.send(All_OrangeRev_YellowNop,All_OrangeRev_YellowNop_LEN,76);
-        delay(WAIT);
-        break;
-      
-      default:
-      Serial.println("INVALID CHAR");
-      break;
-    }
-
-
+  wakeSource = 0;
+  if (!programming_mode){
+    delay(2000);
+    enterSleep();
   }
+
+  // --- Back awake, debounce then handle button ---
+  delay(2000);
+  handleButton(wakeSource);
 }
